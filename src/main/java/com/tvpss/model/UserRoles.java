96ad70d6c63528b61b6f609c8ab656ec882353ab
Zafran Sakowi <110903242+ZafranSY@users.noreply.github.com>
2025-01-03 21:36:49 +0800

    update roles

--- src/main/java/com/tvpss/model/UserRoles.java
+++ src/main/java/com/tvpss/model/UserRoles.java
@@ -1,9 +1,10 @@
 package com.tvpss.model;
 
 public class UserRoles {
-    public static final int SUPER_ADMIN = 1;
-    public static final int ADMIN_PPD = 2;
-    public static final int ADMIN_STATE = 3;
-    public static final int ADMIN_SCHOOL = 4;
-    public static final int STUDENT = 5;
+    public static final int STATE_ADMIN = 1;
+    
+    
+    public static final int ADMIN_SCHOOL = 2;
+    public static final int TEACHER = 3;
+    public static final int STUDENT = 4;
 }
