c2430bd95cb63058e0bfd91618a6533307da17d0
Nor Iman <121821841+mnimannh@users.noreply.github.com>
2025-01-18 02:34:03 +0800

    Revert "ss"
    This reverts commit 4f43b48b24595209ac059f806a1d640ac5d2770e.

--- src/main/java/com/tvpss/controller/adSchoolController.java
+++ src/main/java/com/tvpss/controller/adSchoolController.java
@@ -28,12 +28,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.tvpss.model.Crew;
-import com.tvpss.model.Program;
 import com.tvpss.model.crewTask;
 import com.tvpss.service.CrewService;
-import com.tvpss.service.ProgramService;
 import com.tvpss.service.crewTaskService;
-import com.tvpss.service.ProgramService;
 
 @Controller
 public class adSchoolController {
@@ -43,9 +40,6 @@ public class adSchoolController {
 	
 	@Autowired
 	private crewTaskService crewTaskService;
-	
-	@Autowired
-	private ProgramService programService;
 
 	@GetMapping("/adminschool/dashboard")
 	public String showDashboard(Model model) {
@@ -319,11 +313,5 @@ public class adSchoolController {
 	     }
 	 }
 	 */
-	    @GetMapping("/adminschool/program")
-	    public String listPrograms(Model model) {
-	        List<Program> programs = programService.getAllPrograms();
-	        model.addAttribute("programs", programs);
-	        return "adminschool/program";  
-	    }
 
 }
