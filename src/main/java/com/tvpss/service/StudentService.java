20c9b7f7483b9cb47de350ab3fb2b9147cec07e4
Zafran Sakowi <110903242+ZafranSY@users.noreply.github.com>
2025-01-17 19:53:25 +0800

    Update StudentService.java

--- src/main/java/com/tvpss/service/StudentService.java
+++ src/main/java/com/tvpss/service/StudentService.java
@@ -1,23 +1,30 @@
-//package com.tvpss.service;
-//
-//import com.tvpss.model.crewTask;
-//import com.tvpss.repository.CrewTaskDAO;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import javax.transaction.Transactional;
-//import java.util.List;
-//
-//@Service
-//public class StudentService {
-//	
-//	@Autowired
-//	private CrewTaskDAO crewTaskDAO;
-//	
-//	@Transactional
-//	public crewTask findAlltaskByCrewID(int crewID) {
-//		return crewTaskDAO.findAlltaskByCrewID(crewID);
-//	}
-//	
-//
-//}
+package com.tvpss.service;
+
+import com.tvpss.model.School;
+import com.tvpss.model.crewTask;
+import com.tvpss.repository.CrewTaskDAO;
+import com.tvpss.repository.SchoolDAO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class StudentService {
+	
+	@Autowired
+	private SchoolDAO schoolDAO;
+	
+	@Transactional
+	public List  <School> findAlltaskByCrewID() {
+		return schoolDAO.getAllSchools();
+	}
+	
+	public List <School> getSchoolByDistrict(Long districtID)
+	{
+		return schoolDAO.getSchoolByDistrict(districtID);
+	}
+
+}
